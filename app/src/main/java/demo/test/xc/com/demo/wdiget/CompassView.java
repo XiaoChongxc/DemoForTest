74a74e1ff5a62a671cca9ad4aa33f21e2500e2a9
xc <1234560.xiao>
2020-05-15 18:46:28 +0800

    新增指南针demo

--- app/src/main/java/demo/test/xc/com/demo/wdiget/CompassView.java
+++ app/src/main/java/demo/test/xc/com/demo/wdiget/CompassView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -12,7 +13,6 @@ import android.view.View;
 
 /**
  * 指南针
- *
  */
 public class CompassView extends View {
     private static final String TAG = "CompassView";
@@ -22,28 +22,28 @@ public class CompassView extends View {
         init();
     }
 
-    public CompassView(Context context,  AttributeSet attrs) {
+    public CompassView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
     }
 
-    public CompassView(Context context,  AttributeSet attrs, int defStyleAttr) {
+    public CompassView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
     }
 
-    int  mHeight,mWidth;   //view 的宽高
+    int mHeight, mWidth;   //view 的宽高
     int mRadius;           //指南针的半径， 一般是宽高 /2 -边框
-    float  cX,cY ;//圆心坐标
+    float cX, cY;//圆心坐标
 
     Paint mPaint;
-    Paint  mTextPaint;
+    Paint mTextPaint;
 
-    float  mBorderWidth=6;
-    float  mTextSize=40;
+    float mBorderWidth = 6;
+    float mTextSize = 40;
 
-    private   void  init(){
-        mPaint=new Paint();
+    private void init() {
+        mPaint = new Paint();
         mPaint.setColor(Color.BLACK);
         mPaint.setAntiAlias(true);
         mPaint.setStyle(Paint.Style.STROKE);
@@ -51,21 +51,20 @@ public class CompassView extends View {
 //        mPaint.setPathEffect()
 
 
-        mTextPaint=new Paint();
+        mTextPaint = new Paint();
         mTextPaint.setColor(Color.BLUE);
         mTextPaint.setTextSize(mTextSize);
     }
 
 
-
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-           mHeight=MeasureSpec.getSize(heightMeasureSpec);
-          mWidth=MeasureSpec.getSize(widthMeasureSpec);
-         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        cX=mWidth/2;
-        cY=mHeight/2;
-        mRadius =(Math.min(mHeight,mWidth) - 30) /2;
+        mHeight = MeasureSpec.getSize(heightMeasureSpec);
+        mWidth = MeasureSpec.getSize(widthMeasureSpec);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        cX = mWidth / 2;
+        cY = mHeight / 2;
+        mRadius = (Math.min(mHeight, mWidth) - 30) / 2;
     }
 
 
@@ -77,23 +76,41 @@ public class CompassView extends View {
     }
 
     //画背景的刻度
-    private  void  drawScale(Canvas canvas){
-        Log.d(TAG, "drawScale: ====="+cX+"=="+cY+mRadius);
+    private void drawScale(Canvas canvas) {
+        Log.d(TAG, "drawScale: =====" + cX + "==" + cY + mRadius);
         //画框框
-        canvas.drawCircle(cX,cY,mRadius,mPaint);
+        canvas.drawCircle(cX, cY, mRadius, mPaint);
 
         //画 NSEW
 //        2种方式，  1 直接drawText   2旋转画布
-        canvas.drawText("N",cX,cY-mRadius,mTextPaint);
-        canvas.drawText("S",cX,cY+mRadius,mTextPaint);
-        canvas.drawText("W",cX-mRadius,cY,mTextPaint);
-        canvas.drawText("E",cX+mRadius,cY,mTextPaint);
+        canvas.drawText("N", cX, cY - mRadius +mTextSize +mBorderWidth, mTextPaint);
+        canvas.drawText("S", cX, cY + mRadius-mBorderWidth, mTextPaint);
+        canvas.drawText("W", cX - mRadius+mBorderWidth, cY, mTextPaint);
+        canvas.drawText("E", cX + mRadius-mBorderWidth-mTextSize, cY, mTextPaint);
         //画方位指示背景
+        Path path1 =new Path();
+        path1.moveTo(cX,cY -mRadius*2/3);
+//        path1.lineTo(cX,cY*2/3,cX-100,cY);
+        path1.quadTo(cX,cY -mRadius*2/3,cX-50,cY);
+        path1.quadTo(cX-50,cY,cX,cY +mRadius*2/3);
+
+        path1.quadTo(cX,cY +mRadius*2/3,cX+50,cY);
+        path1.quadTo(cX+50,cY,cX,cY -mRadius*2/3);
+
+
+
+//        path1.quadTo(cX,cY -mRadius*2/3,cX,cY+mRadius*2/3);
+//path1.close();
+
+
+
+        canvas.drawPath(path1,mPaint);
 
 
     }
+
     //画指针
-    private void  drawPointer(Canvas canvas){
+    private void drawPointer(Canvas canvas) {
 
     }
 
